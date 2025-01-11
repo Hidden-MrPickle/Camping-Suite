c0f31bc2a004698e7c01c796899be2f1d65b5347
MrPickle <Hidden_Pickle@protonmail.com>
2025-01-10 19:32:42 -0500

    Transitions and positions

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap
-  .timeline({ repeatDelay: 1, scrolltrigger: {} })
+  .timeline({
+    repeatDelay: 1,
+  })
   .from("h1", {
     opacity: 0,
     y: 300,
@@ -9,5 +11,35 @@ gsap
   .from("p", {
     opacity: 0,
     y: -300,
+    duration: 2,
+  });
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".container",
+      start: "top top",
+      end: "+=150%",
+      scrub: true,
+      pin: true,
+    },
+  })
+  .to("img", {
+    scale: 5,
+    transformOrigin: "top center",
+    ease: "power1.inOut",
+  })
+  .to(".backgroundImage.heroPage", {
+    scale: 1.1,
+    transformOrigin: "center center",
+    ease: "slow(0.7,0.7,false)",
+  })
+  .to("h1", {
+    opacity: 0,
+    y: -300,
+    duration: 2,
+  })
+  .to("p", {
+    opacity: 0,
+    y: 300,
     duration: 2,
   });
